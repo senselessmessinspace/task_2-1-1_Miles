c0d7c6ec9cdc6c153c17c2551b0fcdefc78ff7a8
Oxana Gunchenko <ksuper2007@gmail.com>
2021-08-01 16:03:38 +0300

    Added Miles.java code

---
+++ Miles.java
@@ -0,0 +1,9 @@
+public class Miles {
+    public static void main(String[] args) {
+
+        int ticket_price = 1321;
+        int bonus_mile = ticket_price/20;
+        System.out.println(bonus_mile);
+
+    }
+}
